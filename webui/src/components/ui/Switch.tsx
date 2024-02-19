ff310d0a8fd1d7faeffeec041307b3622d4104de
Farid Mansimli <mensimli222@gmail.com>
2024-02-18 21:43:54 -0500

    nodejs-webrtc-app (1.0.0) ## Sun Feb 18 21:43:54 EST 2024 ##

--- webui/src/components/ui/Switch.tsx
+++ webui/src/components/ui/Switch.tsx
@@ -1,21 +1,20 @@
 interface IProps {
   label: string;
   onChange: (value: boolean) => void;
+  checked: boolean;
 }
 
-const Switch: React.FC<IProps> = ({ label, onChange }) => {
+const Switch: React.FC<IProps> = ({ label, onChange, checked }) => {
   return (
     <label className="relative inline-flex cursor-pointer items-center">
       <input
         type="checkbox"
-        value=""
+        checked={checked}
         className="peer sr-only"
         onChange={(e) => onChange(e.target.checked)}
       />
       <div className="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rtl:peer-checked:after:-translate-x-full dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800"></div>
-      <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
-        {label}
-      </span>
+      <span className="ms-3 text-sm font-medium text-black dark:text-gray-300">{label}</span>
     </label>
   );
 };
--- webui/src/pages/PreviewPage.tsx
+++ webui/src/pages/PreviewPage.tsx
@@ -1,36 +1,35 @@
 import { useState, useEffect } from "react";
 import useNavigation from "../hooks/use-navigation";
-import Link from "../navigation/Link";
 
 import Spinner from "../components/ui/Spinner";
-import * as rtcSocket from "../sockets/webrtc.socket";
 import Switch from "../components/ui/Switch";
 import MyButton from "../components/ui/MyButton";
-import ProfileWidget from "../components/ProfileWidget";
+import * as rtcSocket from "../sockets/webrtc.socket";
+import PeopleWorld from "../components/PeopleWorld";
 
 const PreviewPage = () => {
   const [isSearching, setIsSearching] = useState(false);
   const [socketConnected, setSocketConnected] = useState(false);
-  const [isVideoOn, setIsVideoOn] = useState(false);
+  const [isVideoOn, setIsVideoOn] = useState(true);
 
   const { navigate } = useNavigation();
 
   const meta = Object.fromEntries(new URLSearchParams(location.search));
 
   useEffect(() => {
-    if (socketConnected) {
-      rtcSocket.heyIn(({ pid, pname, deviceType, deviceName }: any) => {
-        const s = { pid, pname, deviceName, deviceType, isVideoOn, first: false, me: meta.me };
-        const params = new URLSearchParams(s as any).toString();
-        navigate("/call?" + params);
-      });
+    if (!socketConnected) return;
 
-      rtcSocket.replyIn(({ pid, pname, deviceType, deviceName }: any) => {
-        const s = { pid, pname, me: meta.me, deviceName, deviceType, isVideoOn, first: true };
-        const params = new URLSearchParams(s as any).toString();
-        navigate("/call?" + params);
-      });
-    }
+    rtcSocket.heyIn(({ pid, pname, deviceType, deviceName }: any) => {
+      const s = { pid, pname, deviceName, deviceType, isVideoOn, first: false, me: meta.me };
+      const params = new URLSearchParams(s as any).toString();
+      navigate("/call?" + params);
+    });
+
+    rtcSocket.replyIn(({ pid, pname, deviceType, deviceName }: any) => {
+      const s = { pid, pname, me: meta.me, deviceName, deviceType, isVideoOn, first: true };
+      const params = new URLSearchParams(s as any).toString();
+      navigate("/call?" + params);
+    });
   }, [socketConnected]);
 
   async function searchOrCancel() {
@@ -56,21 +55,28 @@ const PreviewPage = () => {
   }
 
   return (
-    <div className="flex h-screen w-full">
-      <div className="flex h-screen w-full flex-col items-center justify-between gap-5 border-4 py-[18%]">
-        <ProfileWidget username="Farid Mansimli" profession="Software Developer" />
-        <Link className="text-blue-500" to="/">
-          Go Home
-        </Link>
-        {isSearching ? (
-          <Spinner className="h-20 w-20 border-8" />
-        ) : (
-          <div className="flex items-center justify-center rounded-md border-2 px-10 py-5">
-            <Switch label="video" onChange={(value) => setIsVideoOn(value)} />
+    <div className="h-screen w-full bg-gray-800">
+      <div className="flex h-screen w-full flex-col items-center justify-evenly">
+        <div className="flex w-full flex-col items-center gap-3">
+          <PeopleWorld className="w-[80%] md:w-1/3" />
+        </div>
+        <div className="flex flex-col items-center gap-8">
+          {isSearching ? (
+            <div className="my-8">
+              <Spinner className="h-20 w-20 border-8 md:h-40 md:w-40 md:border-[15px]" />
+            </div>
+          ) : (
+            <div className="flex items-center justify-center rounded-md border-2 px-10 py-5">
+              <Switch
+                label="video"
+                onChange={(value) => setIsVideoOn(value)}
+                checked={isVideoOn}
+              />
+            </div>
+          )}
+          <div>
+            <MyButton onClick={searchOrCancel}>{isSearching ? "cancel" : "search"}</MyButton>
           </div>
-        )}
-        <div className="my-5">
-          <MyButton onClick={searchOrCancel}>{isSearching ? "cancel" : "search"}</MyButton>
         </div>
       </div>
     </div>
