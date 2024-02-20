1551dcbf9b8bb5cc3f6d920462d462b95e0c55f2
Farid Mansimli <mensimli222@gmail.com>
2024-02-20 00:05:46 -0500

    nodejs-webrtc-app (1.0.0) ## Tue Feb 20 00:05:46 EST 2024 ##

--- webui/src/pages/HomePage.tsx
+++ webui/src/pages/HomePage.tsx
@@ -22,7 +22,7 @@ const HomePage = () => {
 
   return (
     <div className="flex w-full flex-col items-center justify-center gap-5 bg-gray-800">
-      <div className="text-xl font-semibold text-indigo-700">HomePage v8</div>
+      <div className="text-xl font-semibold text-indigo-700">HomePage v9</div>
 
       <MyButton onClick={showSearchParams}>show search params</MyButton>
       <MyButton onClick={showSocketData}>show socket data</MyButton>
